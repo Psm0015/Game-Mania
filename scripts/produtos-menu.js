b5a4b7bcc38c1989f73db1fae065fc2e374bb3b1
pedro-mota <pedro.mota@df.estudante.senai.br>
2023-11-03 22:12:13 -0300

    Script com exibição de produtos

---
+++ scripts/produtos-menu.js
@@ -0,0 +1,232 @@
+//////////////////////////////////////////////////////////////////
+// VARIAVEL JSON SIMULANDO DADOS QUE VIRIAM DE UM BANCO DE DADOS//
+//////////////////////////////////////////////////////////////////
+let produtos =
+[
+    {
+        id:1,
+        titulo:"Cadeira Gamer Ergonômica DT3 Sports Gaming Series GT- Blue - Sanbox",
+        preco:849.99,
+        imgPath:"images/Produtos/Cadeira Gamer.png"
+    },
+    {
+        id:2,
+        titulo:"Processador Intel Core i5-12400F, 2.5GHz (4.4GHz Max Turbo)",
+        preco:928.90,
+        imgPath:"images/Produtos/i5 12400f.png"
+    },
+    {
+        id:3,
+        titulo:"Placa de Vídeo PNY NVIDIA GeForce RTX 4060 VERTO Dual Fan",
+        preco:1899.00,
+        imgPath:"images/Produtos/RTX 4060 PNY.png"
+    },
+    {
+        id:4,
+        titulo:"PC GAMER, i5 12400f, 16GB RAM, 3060TI8GB, SSD M.2 480GB",
+        preco:6199.99,
+        imgPath:"images/Produtos/PC GAMER 1.png"
+    },
+    {
+        id:5,
+        titulo:"Processador AMD Ryzen 5 5600, 3.5GHz (4.4GHz Max Turbo)",
+        preco:818.99,
+        imgPath:"images/Produtos/Ryzen 5600.png"
+    },
+    {
+        id:6,
+        titulo:"Headset Gamer Sem Fio Logitech G435 LIGHTSPEED, Conexão USB e Bluetooth",
+        preco:395.00,
+        imgPath:"images/Produtos/Headset G435.png"
+    },
+    {
+        id:7,
+        titulo:"SSD 960 GB Kingston A400, SATA, Leitura: 500MB/s e Gravação: 450MB/s",
+        preco:259.99,
+        imgPath:"images/Produtos/SSD.png"
+    },
+    {
+        id:8,
+        titulo:"Placa de Vídeo RX 6750 XT MECH 2X 12G V1 Radeon, 12GB GDDR6, FreeSync, Dual Fan",
+        preco:2049.99,
+        imgPath:"images/Produtos/RX 6750 XT.png"
+    },
+    {
+        id:9,
+        titulo:"Fonte BRX Rainbow RGB, 750W, 80 Plus Bronze, PFC Ativo",
+        preco:269.90,
+        imgPath:"images/Produtos/Fonte BRX Rainbow RGB.png"
+    },
+    {
+        id:10,
+        titulo:"Gabinete Gamer Ninja Eraser, RGB, Mid Tower, Vidro Temperado, ATX, Com 1 Fan",
+        preco:199.00,
+        imgPath:"images/Produtos/Gabinete.png"
+    },
+    {
+        id:11,
+        titulo:"Water Cooler Gamdias Chione P3-360U, ARGB, 360mm, Display LCD, Intel-AMD",
+        preco:929.90,
+        imgPath:"images/Produtos/Water Cooler.png"
+    },
+    {
+        id:12,
+        titulo:"Headset Gamer Logitech G335",
+        preco:399.90,
+        imgPath:"images/Produtos/Headset G335.png"
+    },
+    {
+        id:13,
+        titulo:"Teclado Gamer Mecânico Logitech G512, Switch Brown, RGB, ABNT2, Carbon",
+        preco:619.99,
+        imgPath:"images/Produtos/Teclado G512.png"
+    },
+    {
+        id:14,
+        titulo:"Cartão De Memória SanDisk, MicroSD 128GB, Classe 10 com Adaptador",
+        preco:189.90,
+        imgPath:"images/Produtos/Cartão De Memória.png"
+    },
+    {
+        id:15,
+        titulo:"Câmera Gopro Hero9 Black À Prova D'água Com Lcd Frontal",
+        preco:1789.00,
+        imgPath:"images/Produtos/GoPro.png"
+    },
+    {
+        id:16,
+        titulo:"Monitor Gamer LG UltraGear 27 Full HD, 144Hz, 1ms, IPS, HDMI e DisplayPort",
+        preco:1099.99,
+        imgPath:"images/Produtos/Monitor LG.png"
+    },
+    {
+        id:17,
+        titulo:"Volante e Pedais Thrustmaster T300 RS GT Edition",
+        preco:2639.99,
+        imgPath:"images/Produtos/Volante.png"
+    },
+    {
+        id:18,
+        titulo:"Oculus Quest 2 Advanced All-in-one Virtual Reality Headset 256GB",
+        preco:3064.25,
+        imgPath:"images/Produtos/Oculos Quest 2.png"
+    },
+    {
+        id:19,
+        titulo:"Console Playstation 5 Sony, SSD 825GB, Controle sem fio DualSense, Edição Digital",
+        preco:3719.91,
+        imgPath:"images/Produtos/PS5.png"
+    },
+    {
+        id:20,
+        titulo:"Console Xbox Series X, 1tb, Preto + Forza Horizon 5",
+        preco:4724.90,
+        imgPath:"images/Produtos/XBOX.png"
+    },
+    {
+        id:21,
+        titulo:"Console Valve Steam Deck 7, 256GB, Nvme 16GB, Preto",
+        preco:4858.06,
+        imgPath:"images/Produtos/Steam Deck.png"
+    },
+    {
+        id:22,
+        titulo:"Placa Mãe Asus ROG STRIX B760-F Gaming Wi-Fi, Intel LGA 1700, ATX, DDR5, Wi-Fi",
+        preco:2999.99,
+        imgPath:"images/Produtos/Plama mae asus ROG.png"
+    },
+    {
+        id:23,
+        titulo:"Memória Kingston Fury Beast, 8GB, 3200MHz, DDR4, CL16, Preto",
+        preco:138.99,
+        imgPath:"images/Produtos/Memoria RAM.png"
+    },
+    {
+        id:24,
+        titulo:"Mousepad Gamer Husky Black Avalanche, Speed, Extra Grande",
+        preco:39.90,
+        imgPath:"images/Produtos/Mouse PAD.png"
+    },
+    
+]
+/////////////////////////////
+// FUNÇÕES DE PREENCHIMENTO//
+/////////////////////////////
+$('#sessao-produtos1').html(produtos1())
+$('#sessao-produtos2').html(produtos2())
+//////////////////////////////////////////////////////
+// FUNÇÃO DE PREENCHER A PRIMEIRA SESSÃO DE PRODUTOS//
+//////////////////////////////////////////////////////
+function produtos1() {
+    var produtosSessao1 = ""
+    min_prd = 1
+    max_prd = 12
+    for (let i = min_prd-1; i < max_prd; i++) {
+        
+        if (i%4 == 0 && i!=0) {
+            produtosSessao1 += 
+`</div>
+<div class="row"> `
+        }else if(i%4 == 0){
+            produtosSessao1 += 
+`<div class="row">`
+        }
+        let valorFormatado = produtos[i].preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        produto = `
+        <div class="col-md-3 mb-4">
+            <div class="card produto">
+                <img src="${produtos[i].imgPath}" class="card-img-top" alt="Produto ${produtos[i].id}">
+                <div class="card-body">
+                    <p class="card-text">${produtos[i].titulo}</p>
+                    <p class="preco">${valorFormatado}</p>
+                </div>
+            </div>
+        </div>
+        `
+        produtosSessao1 += produto
+        if(i == (max_prd)-1){
+            produtosSessao1 += 
+`</div>`
+        }
+    }
+    
+    return produtosSessao1;
+}
+//////////////////////////////////////////////////////
+// FUNÇÃO DE PREENCHER A SEGUNDA SESSÃO DE PRODUTOS//
+//////////////////////////////////////////////////////
+function produtos2() {
+    var produtosSessao2 = ""
+    min_prd = 13
+    max_prd = 24
+    for (let i = min_prd-1; i < max_prd; i++) {
+        
+        if (i%4 == 0 && i!=0) {
+            produtosSessao2 += 
+`</div>
+<div class="row"> `
+        }else if(i%4 == 0){
+            produtosSessao2 += 
+`<div class="row">`
+        }
+        let valorFormatado = produtos[i].preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        produto = `
+        <div class="col-md-3 mb-4">
+            <div class="card produto">
+                <img src="${produtos[i].imgPath}" class="card-img-top" alt="Produto ${produtos[i].id}">
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <p class="card-text">${produtos[i].titulo}</p>
+                    <p class="preco">${valorFormatado}</p>
+                </div>
+            </div>
+        </div>
+        `
+        produtosSessao2 += produto
+        if(i == (max_prd)-1){
+            produtosSessao2 += 
+`</div>`
+        }
+    }
+    
+    return produtosSessao2;
+}
